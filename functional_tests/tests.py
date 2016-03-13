2fc3031ff830fbf9d48ec1f3a31098ed78c9a9b4
jelkand <andersjack@gmail.com>
2016-03-13 11:05:38 -0600

    Expanding FT for multiple users ls

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,8 +1,9 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(3)
@@ -20,8 +21,9 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(input_text)
         inputbox.send_keys(Keys.ENTER)
 
+
     def test_can_start_a_list_and_retrieve_it_later(self):
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
         self.assertIn('To-Do', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-do', header_text)
@@ -32,12 +34,19 @@ class NewVisitorTest(unittest.TestCase):
             'Enter a to-do item'
         )
 
+        #Insert 2 items into table, check if they are there.
         self.input_text_into_list_table('Buy peacock feathers')
+        user1_list_url = self.browser.current_url
+        #Assert REST format for url
+        self.assertRegex(user1_list_url, '/lists/.+')
         self.check_for_row_in_list_table('1: Buy peacock feathers')
         self.input_text_into_list_table('Use peacock feathers to make a fly')
         self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
-        self.fail('Finish test')
+        ##Test for a second user to ensure user lists are distinct.
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
 
-if __name__ == '__main__':
-    unittest.main(warnings='ignore')
+
+
+        self.fail('Finish test')
